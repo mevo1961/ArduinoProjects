db7d0fe26ec2cdc37454827cdee592150b8445d6
Meinolf Vogt <mevo1961@gmail.com>
2017-05-28 18:54:13 +0200

    added new project LEDs_with_Relais

---
+++ LEDs_with_Relais/LEDs_with_Relais.ino
@@ -0,0 +1,36 @@
+#include "Arduino.h"
+
+#define SWITCH 2
+#define JUICE  3
+#define delayTime 20
+//The setup function is called once at startup of the sketch
+void setup()
+{
+	pinMode(SWITCH, OUTPUT);
+	pinMode(JUICE, OUTPUT);
+	digitalWrite(SWITCH, HIGH);
+	analogWrite(JUICE, 0);
+	Serial.begin(9600);
+}
+
+// The loop function is called in an endless loop
+void loop()
+{
+	int i =  0;
+	// one LED on, from 0 to full
+	digitalWrite(SWITCH, HIGH);
+	analogWrite(JUICE, 0);
+
+	for (i = 0; i < 256; i++)
+	{
+		analogWrite(JUICE, i);
+		delay(delayTime);
+	}
+	// now switch to other LED and fade it
+	digitalWrite(SWITCH, LOW);
+	for (i = 255; i >= 0; i--)
+	{
+		analogWrite(JUICE, i);
+		delay(delayTime);
+	}
+}
